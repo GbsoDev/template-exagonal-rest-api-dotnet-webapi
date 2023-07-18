89002e988ae8927c51c7d2487cd894f870f322c7
Gerson Sánchez <gb.so.dev@outlook.com>
2023-07-18 15:27:40 -0500

    WebApi - Core

--- GbsoDevExagonalTemplate.Api/Program.cs
+++ GbsoDevExagonalTemplate.Api/Program.cs
@@ -1,3 +1,6 @@
+using GbsoDevExagonalTemplate.Infrastructure.providers;
+using GbsoDevExagonalTemplate.Infrastructure.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddInfrastructureServices(builder.Configuration, out AppSettings appSettings);
+
 var app = builder.Build();
+
+app.WebApiPosBuild(app.Services, appSettings);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
