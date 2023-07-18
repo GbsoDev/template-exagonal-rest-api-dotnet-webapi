e5b43bda23200c2520c5bce999e656c645909780
Gerson Sánchez <gb.so.dev@outlook.com>
2023-07-18 16:39:19 -0500

    Dtos - Core

---
+++ GbsoDevExagonalTemplate.Dtos/MapperConfigurations/UserMapperConfiguration.cs
@@ -0,0 +1,11 @@
+using GbsoDevExagonalTemplate.Domain.Entities;
+
+namespace GbsoDevExagonalTemplate.Dtos.MapperConfigurations
+{
+	public class UserMapperConfiguration : AutoMapper.Profile
+	{
+		public UserMapperConfiguration()
+		{
+		}
+	}
+}
