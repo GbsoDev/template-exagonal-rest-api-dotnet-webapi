d63ea08a1a078c18c550645ac33b18d12534c12b
Gerson Sánchez <gb.so.dev@outlook.com>
2023-07-18 19:04:37 -0500

    User - Dtos

--- GbsoDevExagonalTemplate.Dtos/MapperConfigurations/UserMapperConfiguration.cs
+++ GbsoDevExagonalTemplate.Dtos/MapperConfigurations/UserMapperConfiguration.cs
@@ -6,6 +6,13 @@ namespace GbsoDevExagonalTemplate.Dtos.MapperConfigurations
 	{
 		public UserMapperConfiguration()
 		{
+			CreateMap<UserDto, User>()
+				.ForMember(x => x.Name, m => m.MapFrom(y => y.Name))
+				.ForMember(x => x.UserName, m => m.MapFrom(y => y.UserName))
+				.ForMember(x => x.Password, m => m.MapFrom(y => y.Password))
+				.ForMember(x => x.Enabled, m => m.Ignore())
+				.ReverseMap()
+				.ForMember(x => x.Password, m => m.Ignore());
 		}
 	}
 }
