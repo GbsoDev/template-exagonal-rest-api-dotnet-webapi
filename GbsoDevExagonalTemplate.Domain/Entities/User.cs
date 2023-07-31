9dee19b1826294a9a48d67028da608c9c64f1d92
Gerson Sánchez <gb.so.dev@outlook.com>
2023-07-31 15:54:49 -0500

    User - Domain

---
+++ GbsoDevExagonalTemplate.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using GbsoDevExagonalTemplate.Domain.Interfaces.Entities;
+
+namespace GbsoDevExagonalTemplate.Domain.Entities
+{
+	public class User : IAuditableEntity<int>
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? UserName { get; set; }
+		public string? Password { get; set; }
+		public bool? Enabled { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public DateTime? UpdatedDate { get; set; }
+	}
+}
---
+++ GbsoDevExagonalTemplate.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using GbsoDevExagonalTemplate.Domain.Entities;
+
+namespace GbsoDevExagonalTemplate.Domain.Interfaces.Repositories
+{
+	public interface IUserRepository : IEntityRepository<User, int>
+	{
+		Task<bool> ValidateUserAsync(string userName, string encript, CancellationToken cancellationToken = default);
+		Task EnableAsync(int id, CancellationToken cancellationToken = default);
+		Task DisableAsync(int id, CancellationToken cancellationToken = default);
+	}
+}
