63b1b70454d3afeec22e17410d3a14357e74fe63
Gerson Sánchez <gb.so.dev@outlook.com>
2023-08-08 17:04:38 -0500

    MSSQL

---
+++ GbsoDevExagonalTemplate.Data.EfCore.MSSQL/MSSQLDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GbsoDevExagonalTemplate.Data.EfCore.MMSQL
+{
+	public sealed class MSSQLDbContext : MainContext<MSSQLDbContext>
+	{
+		public MSSQLDbContext(DbContextOptions<MSSQLDbContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ GbsoDevExagonalTemplate.Data.EfCore.MSSQL/MSSQLDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace GbsoDevExagonalTemplate.Data.EfCore.MMSQL
+{
+	public class MSSQLDbContextFactory : IDesignTimeDbContextFactory<MSSQLDbContext>
+	{
+		public MSSQLDbContext CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<MSSQLDbContext>();
+			var rooConnectionString = BuildConfiguration().GetConnectionString(CONNECTION_DB_NAME);
+			optionsBuilder.UseSqlServer(rooConnectionString);
+			return new MSSQLDbContext(optionsBuilder.Options);
+		}
+
+		public const string CONNECTION_DB_NAME = "migrations-connection-db";
+		public const string FILE_CONFIGURATION_PATH = "appsettings.json";
+
+		private IConfigurationRoot BuildConfiguration()
+		{
+			var configurationResult = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile(FILE_CONFIGURATION_PATH, optional: false, reloadOnChange: true)
+				.Build();
+			return configurationResult;
+		}
+	}
+}
+
