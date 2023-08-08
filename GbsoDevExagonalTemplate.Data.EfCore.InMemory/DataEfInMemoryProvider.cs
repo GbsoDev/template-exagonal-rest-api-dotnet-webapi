ade537cd6551f19be1580c0aedbcbc809975904b
Gerson Sánchez <gb.so.dev@outlook.com>
2023-08-08 17:05:00 -0500

    InMemory

---
+++ GbsoDevExagonalTemplate.Data.EfCore.InMemory/DataEfInMemoryProvider.cs
@@ -0,0 +1,17 @@
+﻿using GbsoDevExagonalTemplate.Data.EfCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GbsoDevExagonalTemplate.Data.EfCore.InMemory
+{
+	public static class DataEfInMemoryProvider
+	{
+		public static IServiceCollection AddDataEfCoreInMemoryDbContext(this IServiceCollection services, string inMemoryDbName)
+		{
+			services.AddDbContext<InMemoryDbContext>(options => options.UseInMemoryDatabase(inMemoryDbName));
+			services.AddScoped<IMainContext, InMemoryDbContext>()
+				.AddScoped(serviceProvider => new Lazy<IMainContext>(() => serviceProvider.GetRequiredService<IMainContext>()));
+			return services;
+		}
+	}
+}
