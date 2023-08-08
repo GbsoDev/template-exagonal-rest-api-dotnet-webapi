bedb1dd6615ecaa5548fa22c52ec57ec45e926e4
Gerson Sánchez <gb.so.dev@outlook.com>
2023-08-08 17:04:38 -0500

    MSSQL

---
+++ GbsoDevExagonalTemplate.Data.EfCore.MSSQL/DataEfMssqlProvider.cs
@@ -0,0 +1,17 @@
+﻿using GbsoDevExagonalTemplate.Data.EfCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GbsoDevExagonalTemplate.Data.EfCore.MMSQL
+{
+	public static class DataEfMssqlProvider
+	{
+		public static IServiceCollection AddDataEfCoreMssqlDbContext(this IServiceCollection services, string connectionString)
+		{
+			services.AddDbContext<MSSQLDbContext>(options => options.UseSqlServer(connectionString));
+			services.AddScoped<IMainContext, MSSQLDbContext>()
+				.AddScoped(serviceProvider => new Lazy<IMainContext>(() => serviceProvider.GetRequiredService<IMainContext>()));
+			return services;
+		}
+	}
+}
